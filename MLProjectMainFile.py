45a1975222dfcc4a8e437c046d27b6a99c858afe
donFrenzel <drf118@scarletmail.rutgers.edu>
2026-04-01 10:22:07 -0400

    Update MLProjectMainFile.py

--- MLProjectMainFile.py
+++ MLProjectMainFile.py
@@ -48,6 +48,15 @@ print(trainingData)
 ### present in the sequence), occurrence (count of each amino acid within the sequence), and bi-gram (frequency of two adjacent amino acids, pair or dipeptide, in the sequence).
 ### so far, the first has been computed and loaded into a csv.  
 
+### Create functions for getting AAC (Count of a given amino acid within the sequence and then divide it by the length of the sequence, do for all and output as a 
+### dict that can then be converted into a dataframe itself.  
+
+### Ocurrence: piggyback off of AAC by just grabbing the pure counts of each; output on the side; count whether an amino acid from the library
+### is present using boolean variables.  All zeroes vector (use np.zeroes()), where it is a single row, each column is a separate amino acid.  
+
+### Bi-Gram: Get bi-gram or 2-gram sequences of each of the Amino Acids in the peptide, i.e. 2 character.  Can use count vectorizer in scikit learn for this, then count
+### and normalize the presence of each of the 2-grams within the sequence.  
+
 ### Third is grouping based on PLMs which can generate numeric encoding of proteins.  (Look into PLM's).  
 
 ### Construct a number of features using a personal approach, iFeature, and another one.  Once those three features are set, use trainingDataLabels to confirm.
